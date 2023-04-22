7ab530932ae6b688b4b9c7532e45c9b8ff489a97
Bruna Nunes <bruna.f.n@hotmail.com>
2023-04-22 19:55:57 +0000

    configurações

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import StartUp from "./app";
 
 // mesma coisa que o index.js
 
-const port = process.env.PORT || "3000";
+const port = process.env.PORT ?? "3000";
 
 StartUp.app.listen(port, function () {
   console.log(`Servidor executando na porta ${port}`);
